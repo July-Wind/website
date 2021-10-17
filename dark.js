6935aa75c0e887e09fdbca7b13c8924f7f4329bf
ShiYi <69226065+July-Wind@users.noreply.github.com>
2021-10-17 17:54:23 +0800

    Update dark.js

--- dark.js
+++ dark.js
@@ -5,7 +5,7 @@
 
 function time(){
 var t = new Date().getHours();
-if (t >= 10 && t <= 6 || t === 10){
+if (t >= 20 && t <= 6 || t === 20){
 var obj = document.getElementById("css");
 obj.setAttribute("href","dark.css");
 }
